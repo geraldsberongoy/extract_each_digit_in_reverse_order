d177dcd3ad0c6ca314e36b1e3bae8592a7187433
geraldsberongoy <geraldsberongoy@iskolarngbayan.pup.edu.ph>
2024-01-23 22:04:37 +0800

    update pseudocode

--- extract_each_digit_in_int_in_reverse_order.py
+++ extract_each_digit_in_int_in_reverse_order.py
@@ -1,7 +1,7 @@
 # Exercise 11: Write a Program to extract each digit from an integer in the reverse order.
 
 # Pseudocode
-# input number
-# convert int to str
-# reverse the number using index
-# for loop to print every numbr with spaces
+# number = 7536
+# number = str(number)
+# reversed = number[::-1]
+# print reverse with .join to add spaces between
